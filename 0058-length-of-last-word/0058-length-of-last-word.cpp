cebfa0041c881698005c5305f696a456d28aa792
Dilnoor Singh <dsingh6_be25@thapar.edu>
2026-08-23 22:38:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.9%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for(int i =s.size()-1;i>=0;i--){
+            if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z'){
+count ++;
+            }
+            else if(count>0){break;}
+        }
+        return count;
+    }
+};
